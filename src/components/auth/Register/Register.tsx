0a23a62324161d039007bc55e4e259e4465d4d1f
exigoll <exigoll@bk.ru>
2023-03-18 15:23:01 +0500

    feat: auth component

---
+++ src/components/auth/Register/Register.tsx
@@ -0,0 +1,105 @@
+import { Button, TextField, Typography } from "@mui/material";
+import React from "react";
+
+import { IPropsRegister } from "@/common/types/auth";
+
+export const Register: React.FC<IPropsRegister> = ({
+  setEmail,
+  setPassword,
+  setRepeatPassword,
+  setFirstName,
+  setUserName,
+  navigate,
+}: IPropsRegister): JSX.Element => {
+  return (
+    <>
+      <Typography
+        sx={{
+          fontFamily: "Poppins",
+          textAlign: "center",
+        }}
+        variant="h3"
+      >
+        Регистрация
+      </Typography>
+      <Typography
+        sx={{
+          fontFamily: "Poppins",
+          textAlign: "center",
+          marginBottom: 3,
+        }}
+        variant="body1"
+      >
+        Введите данные для регистрации
+      </Typography>
+      <TextField
+        margin="normal"
+        fullWidth={true}
+        type="text"
+        label="Имя"
+        placeholder="Введите Ваше имя"
+        variant="outlined"
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        fullWidth={true}
+        type="text"
+        label="Логин"
+        placeholder="Введите Ваш логин"
+        variant="outlined"
+        onChange={(e) => setUserName(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        fullWidth={true}
+        type="email"
+        label="E-mail"
+        placeholder="Введите E-mail"
+        variant="outlined"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        fullWidth={true}
+        type="password"
+        label="Пароль"
+        placeholder="Введите пароль"
+        variant="outlined"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <TextField
+        margin="normal"
+        fullWidth={true}
+        type="password"
+        label="Пароль"
+        placeholder="Повторите пароль"
+        variant="outlined"
+        onChange={(e) => setRepeatPassword(e.target.value)}
+      />
+      <Button
+        sx={{
+          fontFamily: "Poppins",
+          marginTop: 2,
+          marginBottom: 2,
+          width: "60%",
+        }}
+        variant="contained"
+        type="submit"
+      >
+        Регистрация
+      </Button>
+      <Typography
+        variant="body1"
+        sx={{
+          fontFamily: "Poppins",
+        }}
+      >
+        У Вас уже есть аккаунт?
+        <span className="incitingText" onClick={() => navigate("/login")}>
+          Авторизация
+        </span>
+      </Typography>
+    </>
+  );
+};
--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-export const HomePage: React.FC = () => {
+export const Home: React.FC = () => {
   return (
     <div>
       <h1>Home</h1>
